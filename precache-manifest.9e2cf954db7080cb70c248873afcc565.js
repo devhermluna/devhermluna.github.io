78517337edd2888cc157d81ea45a4a5ad4900aca
Herman Luna <hermanluna@pinion.ph>
2019-02-05 16:48:48 +0800

    Updates

---
+++ precache-manifest.9e2cf954db7080cb70c248873afcc565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c266241533188d1e040d",
+    "url": "/static/js/main.c2662415.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c266241533188d1e040d",
+    "url": "/static/css/main.e1ddeaa4.chunk.css"
+  },
+  {
+    "revision": "5776d2daa4e96e3fc8b13fe9a2c11263",
+    "url": "/index.html"
+  }
+];
